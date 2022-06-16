9dd8984b58a333cbf42c1e71d291b998de5a985b
OierGman <75170798+OierGman@users.noreply.github.com>
2022-06-16 13:10:28 +0100

    Flask environment setup

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import requests
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
